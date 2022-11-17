908f42407f59f13d841dd6fa7e1f4091b68b79dc
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-17 23:05:10 +0000

    M something

---
+++ resources/[assets_maps]/[npmlos]/qb_m_gallery/fxmanifest.lua
@@ -0,0 +1,36 @@
+--------------------------------------------------------------------------------------------------
+--                  _     _                 _    _                                    _   _   	--
+--                 | |   (_)        ___    | |  | |            made by               | | | |  	--
+--  ___  __ _ _ __ | |__  _  ___   ( _ )   | |__| | ___  _ __   ___ _   _ _ __  _   _| |_| |_ 	--
+-- / __|/ _` | '_ \| '_ \| |/ _ \  / _ \/\ |  __  |/ _ \| '_ \ / _ \ | | | '_ \| | | | __| __|	--
+-- \__ \ (_| | | | | | | | |  __/ | (_>  < | |  | | (_) | | | |  __/ |_| | | | | |_| | |_| |_ 	--
+-- |___/\__,_|_| |_|_| |_| |\___|  \___/\/ |_|  |_|\___/|_| |_|\___|\__, |_| |_|\__,_|\__|\__|	--
+-- www.twitch.tv/sanhje _/ |               www.twitch.tv/xhoneynuttx __/ |                    	--
+-- 					   |__/                							|___/         				--
+--																								--
+--------------------------------------------------------------------------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Ramees Gallery Interior'
+made_by 'sanhje & Honeynutt'
+
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'sanhje_gallery_timecycle.xml'
+--data_file 'AUDIO_GAMEDATA' 'audio/sanhje_carauction_audio.dat'
+--data_file 'AUDIO_GAMEDATA' 'audio/sanhje_gallery_audio.dat'
+--data_file 'AUDIO_GAMEDATA' 'audio/sanhje_stairs_audio.dat'
+--data_file 'AUDIO_GAMEDATA' 'audio/sanhje_tunnels_audio.dat'
+--data_file 'AUDIO_GAMEDATA' 'audio/sanhje_warehouse_audio.dat'
+
+
+files { 
+    "sanhje_gallery_timecycle.xml",
+    --'audio/sanhje_carauction_audio.dat151.rel',
+    --'audio/sanhje_gallery_audio.dat151.rel',
+    --'audio/sanhje_stairs_audio.dat151.rel',
+    --'audio/sanhje_tunnels_audio.dat151.rel',
+    --'audio/sanhje_warehouse_audio.dat151.rel',
+}
