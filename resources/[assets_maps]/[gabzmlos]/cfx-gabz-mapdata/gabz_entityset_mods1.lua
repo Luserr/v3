8c36a5338e6ea5c32f32c134b71771580d846fab
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-17 19:06:20 +0000

    M6

---
+++ resources/[assets_maps]/[gabzmlos]/cfx-gabz-mapdata/gabz_entityset_mods1.lua
@@ -0,0 +1,326 @@
+local interiors = {
+    {
+        ipl = 'gabz_haters_milo_',
+        coords = { x = -1124.604, y = -1441.168, z = 5.934439 },
+        entitySets = {
+            { name = 'gbz_hatrs_she', enable = true },
+            { name = 'gbz_hatrs_det', enable = true },
+            { name = 'gbz_hatrs_pro', enable = true },
+            { name = 'gbz_hatrs_bis', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_imp_impexp_interior_placement_interior_1_impexp_intwaremed_milo_',
+        coords = { x = 941.00840000, y = -972.66450000, z = 39.14678000 },
+        entitySets = {
+            { name = 'basic_style_set', enable = false },
+            { name = 'urban_style_set', enable = false },
+            { name = 'branded_style_set', enable = true },
+            { name = 'car_floor_hatch', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_biker_milo_',
+        coords = { x = 994.47870000, y = -122.99490000, z = 73.11467000 },
+        entitySets = {
+            { name = 'walls_02', color = 8, enable = true },
+            { name = 'Furnishings_02', color = 8, enable = true },
+            { name = 'decorative_02', enable = true },
+            { name = 'mural_03', enable = true },
+            { name = 'lower_walls_default', color = 8, enable = true },
+            { name = 'mod_booth', enable = true },
+            { name = 'gun_locker', enable = true },
+            { name = 'cash_small', enable = true },
+            { name = 'id_small', enable = true },
+            { name = 'weed_small', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_mrpd_milo_',
+        coords = { x = 451.0129, y = -993.3741, z = 29.1718 },
+        entitySets = {
+            { name = 'v_gabz_mrpd_rm1' , enable = true },
+            { name = 'v_gabz_mrpd_rm2' , enable = true },
+            { name = 'v_gabz_mrpd_rm3' , enable = true },
+            { name = 'v_gabz_mrpd_rm4' , enable = true },
+            { name = 'v_gabz_mrpd_rm5' , enable = true },
+            { name = 'v_gabz_mrpd_rm6' , enable = true },
+            { name = 'v_gabz_mrpd_rm7' , enable = true },
+            { name = 'v_gabz_mrpd_rm8' , enable = true },
+            { name = 'v_gabz_mrpd_rm9' , enable = true },
+            { name = 'v_gabz_mrpd_rm10', enable = true },
+            { name = 'v_gabz_mrpd_rm11', enable = true },
+            { name = 'v_gabz_mrpd_rm12', enable = true },
+            { name = 'v_gabz_mrpd_rm13', enable = true },
+            { name = 'v_gabz_mrpd_rm14', enable = true },
+            { name = 'v_gabz_mrpd_rm15', enable = true },
+            { name = 'v_gabz_mrpd_rm16', enable = true },
+            { name = 'v_gabz_mrpd_rm17', enable = true },
+            { name = 'v_gabz_mrpd_rm18', enable = true },
+            { name = 'v_gabz_mrpd_rm19', enable = true },
+            { name = 'v_gabz_mrpd_rm20', enable = true },
+            { name = 'v_gabz_mrpd_rm21', enable = true },
+            { name = 'v_gabz_mrpd_rm22', enable = true },
+            { name = 'v_gabz_mrpd_rm23', enable = true },
+            { name = 'v_gabz_mrpd_rm24', enable = true },
+            { name = 'v_gabz_mrpd_rm25', enable = true },
+            { name = 'v_gabz_mrpd_rm26', enable = true },
+            { name = 'v_gabz_mrpd_rm27', enable = true },
+            { name = 'v_gabz_mrpd_rm28', enable = true },
+            { name = 'v_gabz_mrpd_rm29', enable = true },
+            { name = 'v_gabz_mrpd_rm30', enable = true },
+            { name = 'v_gabz_mrpd_rm31', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_pillbox_milo_',
+        coords = { x = 311.2546, y = -592.4204, z = 42.32737 },
+        entitySets = {
+            { name = 'rc12b_fixed', enable = false },
+            { name = 'rc12b_destroyed', enable = false },
+            { name = 'rc12b_default', enable = false },
+            { name = 'rc12b_hospitalinterior_lod', enable = false },
+            { name = 'rc12b_hospitalinterior', enable = false },
+        }
+    },
+    {
+        ipl = 'np_tunershop_milo_',
+        coords = { x = 137.7009, y = -3029.3040, z = 11.7953 },
+        entitySets = {
+            { name = 'carlifts', enable = true },
+            { name = 'balcony', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_arcade_milo_',
+        coords = { x = -1653.617, y = -1070.441, z = 11.15339 },
+        entitySets = {
+            { name = 'entity_set_arcade_set_ceiling_beams', enable = true },
+            { name = 'entity_set_arcade_set_ceiling_flat', enable = false },
+            { name = 'entity_set_arcade_set_derelict', enable = false },
+            { name = 'entity_set_arcade_set_streetx4', enable = true },
+            { name = 'entity_set_screens', enable = true },
+            { name = 'entity_set_arcade_set_derelict_clean_up', enable = false },
+            { name = 'entity_set_arcade_set_derelict_carpet', enable = false },
+            { name = 'entity_set_arcade_set_ceiling_mirror', enable = false },
+            { name = 'entity_set_constant_geometry', enable = true },
+            { name = 'entity_set_big_screen', enable = true },
+            { name = 'entity_set_hip_light_no_neon', enable = true },
+            { name = 'entity_set_ret_light_no_neon', enable = false },
+            { name = 'entity_set_arcade_set_trophy_claw', enable = true },
+            { name = 'entity_set_arcade_set_trophy_monkey', enable = true },
+            { name = 'entity_set_arcade_set_trophy_patriot', enable = true },
+            { name = 'entity_set_arcade_set_trophy_brawler', enable = true },
+            { name = 'entity_set_arcade_set_trophy_retro', enable = true },
+            { name = 'entity_set_arcade_set_trophy_racer', enable = true },
+            { name = 'entity_set_arcade_set_trophy_love', enable = true },
+            { name = 'entity_set_arcade_set_trophy_cabs', enable = true },
+            { name = 'entity_set_arcade_set_trophy_gunner', enable = true },
+            { name = 'entity_set_arcade_set_trophy_teller', enable = true },
+            { name = 'entity_set_arcade_set_trophy_strife', enable = true },
+            { name = 'entity_set_arcade_set_trophy_king', enable = true },
+            { name = 'entity_set_plushie_07', enable = true },
+            { name = 'entity_set_plushie_08', enable = true },
+            { name = 'entity_set_plushie_09', enable = true },
+            { name = 'entity_set_plushie_01', enable = true },
+            { name = 'entity_set_plushie_02', enable = true },
+            { name = 'entity_set_plushie_03', enable = true },
+            { name = 'entity_set_plushie_04', enable = true },
+            { name = 'entity_set_plushie_05', enable = true },
+            { name = 'entity_set_plushie_06', enable = true },
+            { name = 'entity_set_arcade_set_plugs_cables', enable = true },
+            { name = 'entity_set_mural_neon_option_01', enable = false },
+            { name = 'entity_set_mural_neon_option_02', enable = false },
+            { name = 'entity_set_mural_neon_option_03', enable = false },
+            { name = 'entity_set_mural_neon_option_04', enable = false },
+            { name = 'entity_set_mural_neon_option_05', enable = false },
+            { name = 'entity_set_mural_neon_option_06', enable = false },
+            { name = 'entity_set_mural_neon_option_07', enable = true },
+            { name = 'entity_set_mural_neon_option_08', enable = false },
+            { name = 'entity_set_mural_option_08', enable = false },
+            { name = 'entity_set_mural_option_07', enable = false },
+            { name = 'entity_set_mural_option_06', enable = false },
+            { name = 'entity_set_mural_option_05', enable = false },
+            { name = 'entity_set_floor_option_08', enable = false },
+            { name = 'entity_set_floor_option_07', enable = false },
+            { name = 'entity_set_floor_option_06', enable = false },
+            { name = 'entity_set_floor_option_05', enable = false },
+            { name = 'entity_set_mural_option_04', enable = false },
+            { name = 'entity_set_mural_option_03', enable = false },
+            { name = 'entity_set_mural_option_01', enable = false },
+            { name = 'entity_set_floor_option_04', enable = false },
+            { name = 'entity_set_floor_option_03', enable = false },
+            { name = 'entity_set_floor_option_02', enable = true },
+            { name = 'entity_set_floor_option_01', enable = false },
+            { name = 'entity_set_mural_option_02', enable = false },
+        }
+    },
+    {
+        ipl = 'gabz_davispd_milo_',
+        coords = { x = 371.0543, y = -1600.378, z = 34.73263 },
+        entitySets = {
+            --{ name = 'davispd_room01_mainhall_ex', enable = true }, --enable this if you want an extra wall before main hall
+            { name = 'davispd_room01_rainhall_es', enable = true },
+            { name = 'davispd_room02_reception_es', enable = true },
+            { name = 'davispd_room03_captainoffice_es', enable = true },
+            { name = 'davispd_room04_officeleft_es', enable = true },
+            { name = 'davispd_room05_officeright_es', enable = true },
+            { name = 'davispd_room06_archives_es', enable = true },
+            { name = 'davispd_room07_staircase_es', enable = true },
+            { name = 'davispd_room08_basementa_es', enable = true },
+            { name = 'davispd_room09_listening_es', enable = true },
+            { name = 'davispd_room10_interrogation_es', enable = true },
+            { name = 'davispd_room11_toilets_es', enable = true },
+            { name = 'davispd_room12_mugshot_es', enable = true },
+            { name = 'davispd_room13_basementb_es', enable = true },
+            { name = 'davispd_room14_armory_es', enable = true },
+            { name = 'davispd_room15_forensics_es', enable = true },
+            { name = 'davispd_room16_lockers_es', enable = true },
+            { name = 'davispd_room17_showerleft_es', enable = true },
+            { name = 'davispd_room18_showerright_es', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_tuner_car_meet_milo_',
+        coords = { x = 1113.89000000, y = -1806.53000000, z = 20.03470000 },
+        entitySets = {
+            { name = 'gabz_carmeet_entset_1', enable = true }, --true
+            { name = 'gabz_carmeet_entset_2', enable = true }, --true
+            { name = 'gabz_carmeet_entset_3', enable = false },
+            { name = 'gabz_carmeet_entset_4', enable = false },
+            { name = 'gabz_carmeet_entset_5', enable = true }, --true
+            { name = 'gabz_carmeet_entset_6', enable = false },
+            { name = 'gabz_carmeet_entset_7', enable = true }, --true
+            { name = 'gabz_carmeet_entset_8', enable = false },
+        }
+    },
+    {
+        ipl = 'gabz_studio_milo_',
+        coords = { x = 487.47000000, y = -72.18980000, z = 57.16330000 },
+        entitySets = {
+            { name = 'gabz_studio_one', enable = true },
+            { name = 'gabz_studio_two', enable = true },
+            { name = 'gabz_studio_three', enable = false },
+            { name = 'gabz_studio_four', enable = false },
+            { name = 'gabz_studio_five', enable = false },
+            { name = 'gabz_studio_six', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_ammu_big_milo_01',
+        coords = { x = 10.90700000, y = -1105.65800000, z = 28.79693000 },
+        entitySets = {
+            { name = 'shooting_range_targets', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_ammu_big_milo_02',
+        coords = { x = 821.14400000, y = -2154.89200000, z = 28.61892000 },
+        entitySets = {
+            { name = 'shooting_range_targets', enable = true },
+        }
+    },
+    {
+        ipl = 'gabz_sm_pb_milo',
+        coords = { x = 260.01200000, y = 220.73300000, z = 108.64600000 },
+        entitySets = {
+            { name = 'room01_entrance', enable = true },
+            { name = 'room02_mainhall', enable = true },
+            { name = 'room03_sideentrance', enable = true },
+            { name = 'room04_roofstairs', enable = true },
+            { name = 'room05_office01', enable = true },
+            { name = 'room06_office02', enable = true },
+            { name = 'room07_office03', enable = true },
+            { name = 'room08_office04', enable = true },
+            { name = 'room09_office05', enable = true },
+            { name = 'room10_office06', enable = true },
+            { name = 'room11_office07', enable = true },
+            { name = 'room12_office08', enable = true },
+            { name = 'room13_mainoffice', enable = true },
+            { name = 'room14_vaultstairs', enable = true },
+            { name = 'room15_vaultbasement', enable = true },
+            { name = 'room16_vault', enable = true },
+            { name = 'room16_vault_trolleys', enable = true }, -- gold and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_paletobank_milo_',
+        coords = { x = -106.037, y = 6467.548, z = 32.169 },
+        entitySets = {
+            { name = 'palbank_vault_trolleys', enable = true }, -- gold and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_fleeca01_milo_',
+        coords = { x = 309.74646, y = -277.644165, z = 53.2345963 },
+        entitySets = {
+            { name = 'room04_cashtrolley', enable = false }, -- bags and cash trolleys
+            { name = 'room05_cashtrolley', enable = false }, -- bags and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_fleeca02_milo_',
+        coords = { x = 1179.74475, y = 2706.985, z = 37.15784 },
+        entitySets = {
+            { name = 'room04_cashtrolley', enable = true }, -- bags and cash trolleys
+            { name = 'room05_cashtrolley', enable = true }, -- bags and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_fleeca03_milo_',
+        coords = { x = 145.416824, y = -1039.277, z = 28.4378834 },
+        entitySets = {
+            { name = 'room04_cashtrolley', enable = true }, -- bags and cash trolleys
+            { name = 'room05_cashtrolley', enable = true }, -- bags and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_fleeca04_milo_',
+        coords = { x = -1216.7616, y = -333.000763, z = 36.85084 },
+        entitySets = {
+            { name = 'room04_cashtrolley', enable = true }, -- bags and cash trolleys
+            { name = 'room05_cashtrolley', enable = true }, -- bags and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_fleeca05_milo_',
+        coords = { x = -355.435852, y = -48.5326, z = 48.1063843 },
+        entitySets = {
+            { name = 'room04_cashtrolley', enable = true }, -- bags and cash trolleys
+            { name = 'room05_cashtrolley', enable = true }, -- bags and cash trolleys
+        }
+    },
+    {
+        ipl = 'gabz_fleeca06_milo_',
+        coords = { x = -2962.59131, y = 478.238037, z = 14.7668953 },
+        entitySets = {
+            { name = 'room04_cashtrolley', enable = true }, -- bags and cash trolleys
+            { name = 'room05_cashtrolley', enable = true }, -- bags and cash trolleys
+        }
+    },
+}
+
+CreateThread(function()
+    for _, interior in ipairs(interiors) do
+        if not interior.ipl or not interior.coords or not interior.entitySets then
+            print('^5[GABZ]^7 ^1Error while loading interior.^7')
+            return
+        end
+        RequestIpl(interior.ipl)
+        local interiorID = GetInteriorAtCoords(interior.coords.x, interior.coords.y, interior.coords.z)
+        if IsValidInterior(interiorID) then
+            for __, entitySet in ipairs(interior.entitySets) do
+                if entitySet.enable then
+                    EnableInteriorProp(interiorID, entitySet.name)
+                    if entitySet.color then
+                        SetInteriorPropColor(interiorID, entitySet.name, entitySet.color)
+                    end
+                else
+                    DisableInteriorProp(interiorID, entitySet.name)
+                end
+            end
+            RefreshInterior(interiorID)
+        end
+    end
+    print("^5[GABZ]^7 Interiors datas loaded.")
+end)
