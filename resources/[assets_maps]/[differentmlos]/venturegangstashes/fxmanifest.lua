712273dac9bfa4d3b603eb27b10a54b52723f241
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-17 18:42:07 +0000

    M4 - getting bored now

---
+++ resources/[assets_maps]/[differentmlos]/venturegangstashes/fxmanifest.lua
@@ -0,0 +1,12 @@
+lua54 'yes'
+
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+      'shellpropsv15.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'shellpropsv15.ytyp'
---
+++ resources/[assets_maps]/[differentmlos]/warungpangku/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'pratyok'
+
+this_is_a_map 'yes'
