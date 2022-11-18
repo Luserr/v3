1fb884b02e84564010097ea86174200b8de16356
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-18 23:07:51 +0000

    More assets removed.

--- resources/[resources]/[qb]/710-lib/sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `710_users` (
-	`pid` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
-	`name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	PRIMARY KEY (`pid`) USING BTREE
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
