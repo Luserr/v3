ba201a1b30088d06a4262aa12d4197460472dde9
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-18 21:45:22 +0000

    Moreee

--- resources/[Rhodinium]/qpixel-business/shared/sh_config.lua
+++
@@ -1,6 +0,0 @@
-Config = Config or {}
-
-Config.BaseStateInterestRatePercent = 10 -- base rate
-Config.StateDebtPercentageIntervals = 50000 -- $50k USD debt = 1% increase on base rate
-Config.DownpaymentRequirement = 35 -- percentage of total amount required by civ
-Config.MaximumCivilianInterestRate = 35 -- max interest % for civs
