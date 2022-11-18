950398d5f4e65da7aa3f6d201bf7db74d96966d0
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-18 23:05:23 +0000

    Removed dumb resource.

--- resources/[resources]/[qb]/qb-firstpersonshooting/config.lua
+++
@@ -1,9 +0,0 @@
-Config = {}
-
-Config.EnteringVehicle = false -- Forced First person if the player is driving a vehicle. (Only the Driver, not the other players.)
-
-Config.Options = {
-    Vehicle = true, -- Forced First Person when you are aiming inside of a vehicle only.
-    ForcedFirst = false, -- Forced First Person when the player is Aiming/Shooting.
-    Bike = true, -- Enables forced first person when Aiming/Shooting On a Bike.
-}
