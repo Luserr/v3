0be6440ef13a468ab2cf011570583a0b04898c67
PAPIMAYO <111662965+PAPIMAYO@users.noreply.github.com>
2022-11-20 07:36:12 -0800

    combined gl-ambulance into qb-ambulance <3
    gl-ambulance and qb-ambulance is now one

--- resources/[resources]/[qb]/[jobs]/qb-ambulancejob/fxmanifest.lua
+++ resources/[resources]/[qb]/[jobs]/qb-ambulancejob/fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.0.0'
 shared_scripts {
 	'@qb-core/shared/locale.lua',
 	'locales/en.lua',
-	'config.lua'
+	'config.lua',
 }
 
 client_scripts {
@@ -16,11 +16,13 @@ client_scripts {
 	'client/laststand.lua',
 	'client/job.lua',
 	'client/dead.lua',
+	'client/glclient.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+	'server/glserver.lua',
+    'server/main.lua',
 }
 
 lua54 'yes'
