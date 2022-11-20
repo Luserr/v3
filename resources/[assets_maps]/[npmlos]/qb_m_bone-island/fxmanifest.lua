e2b7f44fe1132a5d0058d0f099d1da10c2e72b24
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-20 05:42:58 +0000

    New MLO's

---
+++ resources/[assets_maps]/[npmlos]/qb_m_bone-island/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+    'xee_bone_island_timecycle.xml',
+  }
+  
+  data_file 'TIMECYCLEMOD_FILE' 'xee_bone_island_timecycle.xml'
---
+++ resources/[assets_maps]/[npmlos]/qb_m_brofx_mansion09/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game "gta5"
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+files { "stream/interiorproxies.meta" }
+
+this_is_a_map "yes"
+
