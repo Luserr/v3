1a77b11208aad8a1114854eea7fac7bbe9920322
Rhodinium <69939368+Rhodinium@users.noreply.github.com>
2022-11-24 15:03:13 +0000

    Suka Bylat

---
+++ resources/[assets_maps]/[npmlos]/qb_m_russia/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+files {
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'vehicleweapons_mi28.meta',
+    'weaponarchetypes.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'WEAPONINFO_FILE' 'vehicleweapons_mi28.meta'
+data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
